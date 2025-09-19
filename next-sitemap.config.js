91af0ed2a755b80e1c51e2ba114ccaccd78621bb
Vivek Kumar <vivek142000kumar@gamil.com>
2025-09-19 19:03:42 +0530

    Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://vaishalitourism.in',
+  generateRobotsTxt: true, // (optional) generate robots.txt
+  sitemapSize: 5000,       // optional: split sitemap if more than 5000 URLs
+};
